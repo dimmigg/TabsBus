9b36fd73315ca4d17af357d310dc6b82acd15d31
dimmigg <dimmigg@gmail.com>
2017-06-26 16:42:27 +0300

    скролл в теле, немного почистил от хлама(2)

--- app/src/main/java/com/dimka/tabsbus/MainActivity.java
+++ app/src/main/java/com/dimka/tabsbus/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView mTextTab1;
+    private TextView mTextTab2;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setTitle(R.string.title_home);
+                    mTextTab1.setText("home");
+                    mTextTab2.setText("home");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextTab1.setText("dashboard");
+                    mTextTab2.setText("dashboard 2");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextTab1.setText("notifications");
+                    mTextTab2.setText("notifications 2");
                     return true;
             }
             return false;
@@ -37,7 +45,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        // инициализация
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec;
+
+        // создаем вкладку и указываем тег
+        tabSpec = tabHost.newTabSpec("tag1");
+        // название вкладки
+        tabSpec.setIndicator(getString(R.string.TabBudny));
+        // указываем id компонента из FrameLayout, он и станет содержимым
+        tabSpec.setContent(R.id.tvTab1);
+        // добавляем в корневой элемент
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator(getString(R.string.TabVih));
+        tabSpec.setContent(R.id.tvTab2);
+        tabHost.addTab(tabSpec);
+
+
+        // вторая вкладка будет выбрана по умолчанию
+        tabHost.setCurrentTabByTag("tag1");
+
+        // обработчик переключения вкладок
+        tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+            public void onTabChanged(String tabId) {
+                Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mTextTab1 = (TextView) findViewById(R.id.tvTab1);
+        mTextTab2 = (TextView) findViewById(R.id.tvTab2);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
